9d1e8ede85de18294b16349aa677bc09247259b4
Mrhacklx <142010590+Mrhacklx@users.noreply.github.com>
2025-06-13 22:30:54 +0530

    Update info.py

--- info.py
+++ info.py
@@ -13,7 +13,7 @@ BOT_TOKEN = environ.get('BOT_TOKEN', "")
 
 
 # This Pictures Is For Start Message Picture, You Can Add Multiple By Giving One Space Between Each.
-PICS = (environ.get('PICS', 'https://graph.org/file/ce1723991756e48c35aa1.jpg')).split()
+PICS = (environ.get('PICS', 'https://i.ibb.co/0R5MHnpt/what-are-bots-image1-710x400px-300dpi.jpg')).split()
 
 
 # Admins & Users
