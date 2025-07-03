bf946cea81824fb0b08c6b3174d59a45102a1de8
Mrhacklx <142010590+Mrhacklx@users.noreply.github.com>
2025-07-03 09:07:42 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -392,7 +392,7 @@ async def start(client, message):
 
     elif data.split("-", 1)[0] == "refer":
         userid = data.split("-", 2)[1]
-        text = """<b><pre>ʜᴇʏ {message.from_user.mention} 👋,</pre>
+        text = """<b>ʜᴇʏ {message.from_user.mention} 👋,
 Now you can get Premium Membership for FREE!
 
 🔥 Join now, collect coins, and enjoy premium access at no extra cost!
